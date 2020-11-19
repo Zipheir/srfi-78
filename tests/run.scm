716646a3dfeb51ffab342b160777147e0a381346
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-19 09:42:33 -0800

    Added first test of check-ec, which is expected to fail

--- tests/run.scm
+++ tests/run.scm
@@ -1,5 +1,6 @@
 (import scheme)
 (import (chicken base))
+(import srfi-42)
 (import srfi-78)
 
 (include "srfi-78-tests.scm")
--- tests/srfi-78-tests.scm
+++ tests/srfi-78-tests.scm
@@ -1,1 +1,14 @@
 (check (+ 1 1) => 2)
+
+;; The following test is expected to fail
+;;
+;; Per the SRFI-78 documentation:
+;;
+;; "The output could look like this:
+;;
+;; (let ((e 53) (x 9007199254740992.0)) (= (+ x 1) x)) => #t ; *** failed ***
+;;  ; expected result: #f
+;;
+(check-ec (:range e 100)
+          (:let x (expt 2.0 e))
+          (= (+ x 1) x) => #f (e x))
